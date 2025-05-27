67e8b4ede2664079086b3ab31ff761b834511c44
nngu0110 <nngu0110@uni.sydney.edu.au>
2025-05-28 09:35:06 +1000

    adding code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,32 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>HelloWorld</h1>
+ return (
+  <>
+   <nav className="bg-gray-800 p-4">
+    <ul className="flex space-x-4">
+     <li>
+      <Link className="text-white hover:text-gray-400" href="/dashboard">
+       Dashboard
+      </Link>
+     </li>
+     <li>
+      <Link className="text-white hover:text-gray-400" href="/building">
+       Building
+      </Link>
+     </li>
+     <li>
+      <Link className="text-white hover:text-gray-400" href="/customer">
+       Customer
+      </Link>
+     </li>
+     <li>
+      <Link className="text-white hover:text-gray-400" href="/pricing">
+       Pricing
+      </Link>
+     </li>
+    </ul>
+   </nav>
+  </>
+ );
 }
